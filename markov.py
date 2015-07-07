f997f158995e48b187bc872a3575e1e150163d2a
Hackbright Student <info@hackbrightacademy.com>
2015-07-07 12:16:06 -0700

    got the dictionary working

--- markov.py
+++ markov.py
@@ -13,35 +13,23 @@ def make_chains(corpus):
         source_text.append(split_text)
     i = 0
     for item in source_text:
-        
         concat_list = concat_list + source_text[i]
         i += 1
 
     bi_gram_dictionary = {}
     index = 0
-    while index < (len(concat_list) - 2):
 
+    while index < (len(concat_list) - 2):
         word_pair = (concat_list[index], concat_list[index + 1])
-        #print word_pair
         if word_pair in bi_gram_dictionary:      
-            bi_gram_dictionary[word_pair].append(concat_list[i + 2])
-            #print bi_gram_dictionary[word_pair]
+            bi_gram_dictionary[word_pair].append(concat_list[index + 2])
         else:
-            bi_gram_dictionary[word_pair] = [concat_list[i + 2]]
-            #print word_pair
+            bi_gram_dictionary[word_pair] = [concat_list[index + 2]]
         index += 1
 
-    print bi_gram_dictionary
-    # for tri-grams in source_text:
-    #     i = 0
-    #     create values that are sets of single words that follow established bi-grams
-    #     word by word, index by index
-    #     i += 0
-
-
     original_text.close()
+    return bi_gram_dictionary
 
-make_chains("green-eggs.txt")
 
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
